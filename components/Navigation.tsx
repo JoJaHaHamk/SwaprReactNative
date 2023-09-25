6cb16498a4dfee692880d9daba739e78344ae932
jari.valentine <jari.valentine@student.howest.be>
2023-09-25 17:26:15 +0300

    added navigation compenent

---
+++ components/Navigation.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Colors } from '../constants/values';
+
+const Navigation = () => {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+export default Navigation;
